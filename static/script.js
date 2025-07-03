5e79c610838ca2158b4be7f6f30398c2210bcddc
Lovro <lovroperaic@gmail.com>
2025-07-03 12:47:45 +0200

    Implement JS

--- static/script.js
+++ static/script.js
@@ -1,3 +1,18 @@
+const form = document.querySelector('form');
+const ratingCard = document.querySelector('.card#rating');
+const thanksCard = document.querySelector('.card#thanks');
+const ratingField = document.querySelector('#selected-rating');
+
 document.addEventListener('DOMContentLoaded', () => {
-    
+    form.addEventListener('submit', handleSubmit);
 })
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const rating = Object.fromEntries(new FormData(event.target)).rating;
+    if (rating) {
+        ratingField.textContent = rating;
+        ratingCard.setAttribute('hidden', '');
+        thanksCard.removeAttribute('hidden');
+    }
+}
